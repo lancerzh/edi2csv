147f54fbaca0c774f9dc0de13630a4649081c2ad
lancerzh <xlancer@yeah.net>
2015-05-30 22:44:01 -0400

    redesign config.ini file it is work

--- src/csv2edi.py
+++ src/csv2edi.py
@@ -4,7 +4,7 @@ Created on May 18, 2015
 @author: lancer
 '''
 
-import csv
+from re import match, IGNORECASE
 import ConfigParser
 
 import x12edi
@@ -62,6 +62,7 @@ def replaceSubmitterReceiver(template, config):
 def proc(template, csvdb, config):
     # do insert
     for (k, v) in config.items('insert segment after'):
+        print k, v
         (loopname, segmentPattern) = k.split('/')
         loops = template.fetchSubNodes(loopname);
         for loop in loops:
@@ -70,7 +71,7 @@ def proc(template, csvdb, config):
     lxLoop = template.fetchSubNodes("LX");
     for lx in lxLoop :
         conditions = []
-        for (k, v) in config.items('keys'):
+        for (k, v) in config.items('keymap'):
             docValue = lx.getValue(k);
             conditions.append((v, docValue));
             #print k, v, docValue
@@ -78,12 +79,12 @@ def proc(template, csvdb, config):
         results = csvdb.search(conditions) ; 
         if len(results) > 1 :
             print "err: expect 1, but get " +  len(results) + " results. Check csv file or keys!"
-            for (k, v) in config.items('keys'):
+            for (k, v) in config.items('keymap'):
                 print k, v
             break;
         if len(results) == 0:
             print "Not found any result."
-            for (k, v) in config.items('keys'):
+            for (k, v) in config.items('keymap'):
                 print k, v
             continue;
         result = results[0];
@@ -91,13 +92,13 @@ def proc(template, csvdb, config):
         # do replace
         for (k, v) in config.items('replace element'):
             #print k, v;
-            value = result.getValue(v);
+            value = result.getValue(v.split(':')[1]);
             lx.replaceValue(value, k);
         
         # do append
         for (k, v) in config.items('append element'):
             #print k, v;
-            value = result.getValue(v);
+            value = result.getValue(v.split(':')[1]);
             lx.appendValue(value, k);
             
         recalc_LX_HCP(lx)
@@ -116,12 +117,57 @@ def proc(template, csvdb, config):
             
     return template.isaNode.dump();
 
+def check(config):
+    reInsert = r'insert\s+([\'"]?\S+~[\'"]?)\s+after\s+(\S+)';
+    reSetvalue = r'setvalue\s+(\$\{csv:[\w]+\})\s+into (\S+)'
+    reAppend = r'append\s+(\$\{csv:[\w]+\})\s+at\s+(\S+)'
+    reText = r'[\'"]?[^\$][\w]+[\'"]?'
+    reVar = r'[\'"]?\$\{[\w]+\}[\'"]?'
+    reCsv = r'[\'"]?\$\{csv:[\w]+\}[\'"]?'
+
+    config.add_section('insert segment after');
+    config.add_section('replace element');
+    config.add_section('append element');
+
+    for (k, v) in config.items('actions'):
+        kint = int(k);
+        #print v
+        matchObj = match(reInsert, v);
+        if matchObj != None:
+            template = matchObj.group(1).strip(r'[\'" ]')
+            locator = matchObj.group(2)
+            print template, locator
+            config.set('insert segment after', locator, template);
+            continue;
+        matchObj = match(reSetvalue, v);
+        if matchObj != None:
+            #print v
+            template = matchObj.group(1).strip(r'[\$\{\}\s]')
+            locator = matchObj.group(2)
+            config.set('replace element', locator, template);
+            continue;
+        matchObj = match(reAppend, v);
+        if matchObj != None:
+            template = matchObj.group(1).strip(r'[\$\{\}\s]')
+            locator = matchObj.group(2)
+            config.set('append element', locator, template);
+            continue;
+        else :
+            
+            pass
+        '''
+        if match(reText, v):
+        if match(reCsv, v):
+        '''
+
+    pass
 
 
     
 if __name__ == '__main__':
     config = ConfigParser.RawConfigParser()
     config.read('from_reliant_csv.ini');
+    check(config);
     ediTemplate = None;
     ediTemplateFile = 'extras/testdata/RHPPAI_0505151403_3.txt';
     with open(ediTemplateFile, 'rb') as edifile:
@@ -132,6 +178,7 @@ if __name__ == '__main__':
     csvfilename = 'extras/testdata/JMS_RELIANT_I_20150505output.csv'
     with open(csvfilename, 'rb') as csvfile:
         csvDb = csvwrapper.CsvDatabase(csvfile, skip = 1);
+        csvfile.close();
 
     
     r = proc(ediTemplate, csvDb, config);
--- tests/csv2ediTest.py
+++ tests/csv2ediTest.py
@@ -4,6 +4,7 @@ Created on May 18, 2015
 @author: lancer
 '''
 import unittest
+from re import match, IGNORECASE;
 
 import x12edi
 import csv2edi
@@ -12,7 +13,42 @@ import csv2edi
 class Test(unittest.TestCase):
 
 
-    def testName(self):
+    def testMatch(self):
+        reInsert = r'insert\s+([\'"]?\S+~[\'"]?)\s+after\s+(\S+)';
+        self.assertIsNotNone(match(reInsert, "insert  'NTE*ADD*~' after LX/K3"))
+        self.assertIsNotNone(match(reInsert, 'insert "NTE*ADD*~"  after LX/K3'))
+        self.assertIsNotNone(match(reInsert, "insert  NTE*ADD*~  after  LX/K3"))
+        self.assertIsNotNone(match(reInsert, "INSERT \t 'NTE*ADD*~' AFTER LX/K3", IGNORECASE))
+
+        reSetvalue = r'setvalue\s+(\$\{csv:[\w]+\})\s+into (.+)'
+        self.assertIsNotNone(match(reSetvalue, 'setvalue ${csv:k} into LX/HCP*02/02'));
+        
+        reAppend = r'append\s+(csv:[\w]+)\s+at\s+(.+)'
+        self.assertIsNotNone(match(reAppend, 'append csv:m at LX/NTE*ADD/02,01'));
+        
+        reText = r'[\'"]?[^\$][\w]+[\'"]?'
+        self.assertIsNotNone(match(reText, 'aStringText'));
+        self.assertIsNotNone(match(reText, '"aStringText"'));
+        self.assertIsNotNone(match(reText, "'aStringText'"));
+        self.assertIsNone(match(reText, "'$aStringText'"));
+        self.assertIsNone(match(reText, "'${aStringText}'"));
+        
+        reVar = r'[\'"]?\$\{[\w]+\}[\'"]?'
+        self.assertIsNotNone(match(reVar, '${aVar}'));
+        self.assertIsNotNone(match(reVar, '"${aVar}"'));
+        self.assertIsNotNone(match(reVar, "'${aVar}'"));
+        self.assertIsNone(match(reVar, "'aVar'"));
+        self.assertIsNone(match(reVar, "'{aVar}'"));
+
+
+        reCsv = r'[\'"]?\$\{csv:[\w]+\}[\'"]?'
+        self.assertIsNotNone(match(reCsv, '${csv:k}'));
+        self.assertIsNotNone(match(reCsv, '"${csv:k}"'));
+        self.assertIsNotNone(match(reCsv, "'${csv:k}'"));
+        self.assertIsNotNone(match(reCsv, '${csv:kk}'));
+        self.assertIsNotNone(match(reCsv, '${csv:KK}'));
+        self.assertIsNotNone(match(reCsv, '${csv:0}'));
+        self.assertIsNotNone(match(reCsv, '${csv:10}'));
 
         pass
     
